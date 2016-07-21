c732194c3129afa2d3e1f96d036c142429314909
Chen Yang <iamyangchen@gmail.com>
2016-07-20 23:07:23 -0500

    added ios ui target

---
+++ ibr-dtn/ibrdtn-ui/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ibrdtn-ui
+//
+//  Created by Chen Yang on 7/20/16.
+//  Copyright © 2016 Chen Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
